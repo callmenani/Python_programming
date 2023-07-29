cc0706a612f5097ddee30499e53a9d8293e6fd86
Harsha Vardhan R <92045868+callmenani@users.noreply.github.com>
2023-07-29 15:11:03 +0530

    Add files via upload

---
+++ Working with variables/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city_name = input("What's the name of your city : ")
+pet_name = input("What's the name of your pet : ")
+print("Your Band name is : " + city_name + " " + pet_name)
+
