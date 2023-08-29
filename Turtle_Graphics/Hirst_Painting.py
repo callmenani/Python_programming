79cb579eb3c0132e2516c3c166e64654ef52d787
Harsha Vardhan R <92045868+callmenani@users.noreply.github.com>
2023-08-29 14:57:35 +0530

    Add files via upload

---
+++ Turtle_Graphics/Hirst_Painting.py
@@ -0,0 +1,29 @@
+import turtle as turtle_module 
+import random 
+
+screen = turtle_module.Screen()
+
+turtle_module.colormode(255)
+timmy = turtle_module.Turtle()
+timmy.speed("fastest")
+timmy.penup()
+timmy.hideturtle()
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+num_of_dots = 100
+
+for i in range(1, num_of_dots+1):
+    timmy.dot(20 , random.choice(color_list))
+    timmy.forward(50)
+    
+    if i%10 == 0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
+
+
+screen.exitonclick()
